2618cdfc3527ff943ddffb4acaaccdcc83504314
NunesAzevedo <pnazevedo@gmail.com>
2024-05-15 11:16:43 -0300

    Consegui fazer o código funcionar para 1 quadrante

--- labirinto_de_pascal/grade/main.py
+++ labirinto_de_pascal/grade/main.py
@@ -13,36 +13,36 @@ linedrawer = turtle.Turtle() # Linha estática que fica no nível das letras
 
 #Declaração das letras, que representam os possíveis destinos dos caminhos
 
-a  = turtle.Turtle()
-b  = turtle.Turtle()
+# a  = turtle.Turtle()
+# b  = turtle.Turtle()
 c  = turtle.Turtle()
 d  = turtle.Turtle()
 e  = turtle.Turtle()
-f  = turtle.Turtle()
-g  = turtle.Turtle()
+# f  = turtle.Turtle()
+# g  = turtle.Turtle()
 h  = turtle.Turtle()
 i  = turtle.Turtle()
 j  = turtle.Turtle()
-k  = turtle.Turtle()
-l  = turtle.Turtle()
-m  = turtle.Turtle()
+# k  = turtle.Turtle()
+# l  = turtle.Turtle()
+m  = turtle.Turtle() #Origem
 n  = turtle.Turtle()
 o  = turtle.Turtle()
-p  = turtle.Turtle()
-q  = turtle.Turtle()
-r  = turtle.Turtle()
-s  = turtle.Turtle()
-t  = turtle.Turtle()
-u  = turtle.Turtle()
-v  = turtle.Turtle()
-w  = turtle.Turtle()
-w2 = turtle.Turtle()
-w3 = turtle.Turtle()
+# p  = turtle.Turtle()
+# q  = turtle.Turtle()
+# r  = turtle.Turtle()
+# s  = turtle.Turtle()
+# t  = turtle.Turtle()
+# u  = turtle.Turtle()
+# v  = turtle.Turtle()
+# w  = turtle.Turtle()
+# w2 = turtle.Turtle()
+# w3 = turtle.Turtle()
 
 
 ############ FIM - Declaração dos icones do mapa ############
 
-t.speed(0)
+m.speed(0) # Setado na origem
 
 dorian.hideturtle(), linedrawer.hideturtle() # "Esconder" inicialmente o boneco enquanto ele não é devidamente setado no mapa
 
@@ -69,35 +69,35 @@ linedrawer.speed(0)
 
 # Animação, onde cada letra parte de um mesmo ponto, e vai em direção ao seu local no mapa
 
-a.penup(),  a.goto(-200,  200),  a.left(90),  a.pendown()
-b.penup(),  b.goto(-100,  200),  b.left(90),  b.pendown()
+# a.penup(),  a.goto(-200,  200),  a.left(90),  a.pendown()
+# b.penup(),  b.goto(-100,  200),  b.left(90),  b.pendown()
 c.penup(),  c.goto(   0,  200),  c.left(90),  c.pendown()
 d.penup(),  d.goto( 100,  200),  d.left(90),  d.pendown()
 e.penup(),  e.goto( 200,  200),  e.left(90),  e.pendown()
 
-f.penup(),  f.goto(-200, 100),  f.left(90),  f.pendown()
-g.penup(),  g.goto(-100, 100),  g.left(90),  g.pendown()
+# f.penup(),  f.goto(-200, 100),  f.left(90),  f.pendown()
+# g.penup(),  g.goto(-100, 100),  g.left(90),  g.pendown()
 h.penup(),  h.goto(   0, 100),  h.left(90),  h.pendown()
 i.penup(),  i.goto( 100, 100),  i.left(90),  i.pendown()
 j.penup(),  j.goto( 200, 100),  j.left(90),  j.pendown()
 
-k.penup(),  k.goto(-200,    0),  k.left(90),  k.pendown()
-l.penup(),  l.goto(-100,    0),  l.left(90),  l.pendown()
+# k.penup(),  k.goto(-200,    0),  k.left(90),  k.pendown()
+# l.penup(),  l.goto(-100,    0),  l.left(90),  l.pendown()
 m.penup(),  m.goto(   0,    0),  m.left(90),  m.pendown() #Origem
 n.penup(),  n.goto( 100,    0),  n.left(90),  n.pendown()
 o.penup(),  o.goto( 200,    0),  o.left(90),  o.pendown()
 
-p.penup(),  p.goto(-200, -100),  p.left(90),  p.pendown()
-q.penup(),  q.goto(-100, -100),  q.left(90),  q.pendown()
-r.penup(),  r.goto(   0, -100),  r.left(90),  r.pendown()
-s.penup(),  s.goto( 100, -100),  s.left(90),  s.pendown()
-t.penup(),  t.goto( 200, -100),  t.left(90),  t.pendown()
+# p.penup(),  p.goto(-200, -100),  p.left(90),  p.pendown()
+# q.penup(),  q.goto(-100, -100),  q.left(90),  q.pendown()
+# r.penup(),  r.goto(   0, -100),  r.left(90),  r.pendown()
+# s.penup(),  s.goto( 100, -100),  s.left(90),  s.pendown()
+# t.penup(),  t.goto( 200, -100),  t.left(90),  t.pendown()
 
-u.penup(),  u.goto(-200, -200),  u.left(90),  u.pendown()
-v.penup(),  v.goto(-100, -200),  v.left(90),  v.pendown()
-w.penup(),  w.goto(   0, -200),  w.left(90),  w.pendown()
-w2.penup(), w2.goto( 100, -200), w2.left(90), w2.pendown()
-w3.penup(), w3.goto( 200, -200), w3.left(90), w3.pendown()
+# u.penup(),  u.goto(-200, -200),  u.left(90),  u.pendown()
+# v.penup(),  v.goto(-100, -200),  v.left(90),  v.pendown()
+# w.penup(),  w.goto(   0, -200),  w.left(90),  w.pendown()
+# w2.penup(), w2.goto( 100, -200), w2.left(90), w2.pendown()
+# w3.penup(), w3.goto( 200, -200), w3.left(90), w3.pendown()
 
 
 
@@ -119,16 +119,45 @@ for i_ in range (0, 50):
         coin=random.randint(0,100) #coin: valor aleatório entre 0 a 100 que será comparado com o valor de rand
         if coin > rand:
             x += 100
-            dorian.pendown(), dorian.speed(6), dorian.color("red"), dorian.goto(x,y)
+            dorian.pendown(), dorian.speed(3), dorian.color("red"), dorian.goto(x,y)
         #t.clear()
         else:
             y += 100
-            dorian.pendown(), dorian.speed(6), dorian.color("blue"), dorian.goto(x,y)
+            dorian.pendown(), dorian.speed(3), dorian.color("blue"), dorian.goto(x,y)
 
         count += 1
 
 
         # Registrando em qual letra dorian chegou
+
+        if   x ==    0 and y ==    0:
+            m.forward(5)
+        elif x ==  100 and y ==    0:
+            n.forward(5)
+        elif x ==  200 and y ==    0:
+            o.forward(5)
+        elif x ==    0 and y ==  100:
+            h.forward(5)
+        elif x ==  100 and y ==  100:
+            i.forward(5)
+        elif x ==  200 and y ==  100:
+            j.forward(5)
+        elif x ==    0 and y ==  200:
+            c.forward(5)
+        elif x ==  100 and y ==  200:
+            d.forward(5)
+        elif x ==  200 and y ==  200:
+            e.forward(5)
+        
+        if x == 200 or y == 200:
+            x = 0
+            y = 0
+            dorian.clear()
+
+
+
+
+
 
         # if x ==   -200 and y ==  200:
         #     a.forward(5)
@@ -186,10 +215,10 @@ for i_ in range (0, 50):
         #     w3.forward(5)
 
 
-        if (abs(x) + abs(y) ) == 400:
-            x = 0
-            y = 0
-            dorian.clear() # Tira o dorian da tela
+        # if (abs(x) + abs(y) ) == 400:
+        #     x = 0
+        #     y = 0
+        #     dorian.clear() # Tira o dorian da tela
 
 
 
